135d68226534e197aa36bd2c17560fa6adc14dc6
Felix Chin <felixmchin@gmail.com>
2021-01-30 22:08:33 -0800

    added comments to index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// iterates through i until n input is reached
+// if i modulus 15 is equal to 0, print out 'CLXDTC'
+// if i modulus 3 is equal to 0, print out 'CLX'
+// if i modulus 5 is equal to 0, print out 'DTC'
+// otherwise print out the current iteration of i
 const clxDTC = (n) => {
   for (let i = 0; i <= n; i++) {
     if (i % 15 === 0) {
@@ -12,6 +17,6 @@ const clxDTC = (n) => {
   }
 }
 
+// call the function with input of 100
 const n = 100;
-
 clxDTC(n);
